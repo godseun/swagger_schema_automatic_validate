9f886bd8db8272ceb82cc4cd06a2bfb1e6516224
godseun_mac <alcy1024@gmail.com>
2023-07-18 12:26:31 +0900

    실패 테스트케이스 추가

--- test/Pet.spec.ts
+++ test/Pet.spec.ts
@@ -1,23 +1,30 @@
 import { extendJSCMatcher } from "../src/jestExt";
 import petSchema from "../__schema__/PetSchema.json";
+import apiResponseSchema from "../__schema__/ApiResponseSchema.json";
+
+import type { Pet, ApiResponse } from "../__apiTypesTemp__";
 
 extendJSCMatcher();
 
-// const PET_ID = 1;
-function fetchPetId(petId: number) {
-  return fetch(`https://petstore.swagger.io/v2/pet/${petId}`).then((res) =>
-    res.json()
+async function fetchPetId(petId: number): Promise<Pet | ApiResponse> {
+  return await fetch(`https://petstore.swagger.io/v2/pet/${petId}`).then(
+    (res) => res.json()
   );
 }
 
 describe("Pet api Pet Group", () => {
   describe("pet/{petId} api", () => {
     it("정상 처리(200)", async () => {
-      // expect(PET_ID).toEqual(1);
       const data = await fetchPetId(2);
 
       // @ts-ignore
       expect(petSchema).toMatchJSC(data);
+    });
+    it("실패 처리(400)", async () => {
+      const data = await fetchPetId(-1);
+
+      // @ts-ignore
+      expect(apiResponseSchema).toMatchJSC(data);
     });
   });
 });
